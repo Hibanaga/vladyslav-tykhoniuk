03c9183e8bf8706b6b4c6b9c863c88f64603c07d
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-12-29 12:00:32 +0100

    Finish navigation bar of left side of website

--- src/components/modules/NavigationBar/index.tsx
+++ src/components/modules/NavigationBar/index.tsx
@@ -2,6 +2,10 @@
 
 import React, { FC } from 'react';
 
+import Link from 'next/link';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 import StyledComponent from './styles';
 
 interface Props {}
@@ -13,22 +17,42 @@ const ModuleNavigationBar: FC<Props> = ({ }) => {
 
     return (
         <StyledComponent className='module-navigation-bar'>
-            <a href="/" className='headline'>Tykhoniuk Vladyslav</a>
-            <h3 className='sub-headline'>Full-stack developer</h3>
+            <nav className='nav'>
+                <header className='header'>
+                    <a href="/" className='headline'>Tykhoniuk Vladyslav</a>
+                    <h3 className='sub-headline'>Full-stack developer</h3>
 
-            <span className='additional'>I build exceptional and accessible digital experiences for the web.</span>
+                    <span className='additional'>I build exceptional and accessible digital experiences for the web.</span>
 
-            <ul className='nav-list'>
-                <li className='nav-item'>
-                    <a href="about:blank" className='link' onClick={onClick}>about</a>
-                </li>
-                <li className='nav-item'>
-                    <a href="about:blank" className='link' onClick={onClick}>experience</a>
-                </li>
-                <li className='nav-item'>
-                    <a href="about:blank" className='link' onClick={onClick}>projects</a>
-                </li>
-            </ul>
+                    <ul className='nav-list'>
+                        <li className='nav-item'>
+                            <a href="about:blank" className='link' onClick={onClick}>about</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a href="about:blank" className='link' onClick={onClick}>experience</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a href="about:blank" className='link' onClick={onClick}>projects</a>
+                        </li>
+                    </ul>
+                </header>
+
+                <footer className='footer'>
+                    <ul className='list'>
+                        <li className='list-item'>
+                           <Link href='https://www.linkedin.com/in/vlad-tykhoniuk/' target="_blank" className='link'>
+                               <LinkedInIcon className='icon' />
+                           </Link>
+                        </li>
+
+                        <li className='list-item'>
+                            <Link href='https://github.com/Hibanaga' target="_blank" className='link'>
+                                <GitHubIcon className='icon gh' />
+                            </Link>
+                        </li>
+                    </ul>
+                </footer>
+            </nav>
         </StyledComponent>
     );
 };
--- src/components/modules/NavigationBar/styles.ts
+++ src/components/modules/NavigationBar/styles.ts
@@ -3,72 +3,119 @@
 import styled from "styled-components";
 
 export default styled.aside`
-    position: sticky;
-    top: 5em;
-    max-height: 17.5em;
-
-    .headline {
-        font-size: 2em;
-        margin-bottom: .5em;
-        font-weight: 700;
-        color: #E2E8F0;
-    }
+    position: relative;
     
-    .sub-headline {
-        font-size: 1.25em;
-        font-weight: 400;
-        margin-bottom: 1em;
-        color: #E2E8F0;
-    }
-    
-    .additional {
-        display: block;
-        font-weight: 300;
-        font-size: .85em;
-        margin-bottom: 3em;
-        max-width: 60%;
-        line-height: 150%;
-        color: #94A3B8;
-    }
-    
-    .nav-list {
-        .nav-item {
-            list-style: none;
-            margin-bottom: .5em;
+    .nav {
+        position: sticky;
+        top: 5em;
+        max-height: 72.5vh;
+        height: 100%;
+        
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        
+        .header {
+            padding-bottom: 3em;
             
-            .link {
-                position: relative;
-
-                cursor: pointer;
-
-                color: #64748B;
-                font-size: .65em;
+            .headline {
+                font-size: 2em;
+                margin-bottom: .5em;
                 font-weight: 700;
-                letter-spacing: .15em;
+                color: #E2E8F0;
+            }
 
-                padding: 1em .5em 1em 3em;
+            .sub-headline {
+                font-size: 1.25em;
+                font-weight: 400;
+                margin-bottom: 1em;
+                color: #E2E8F0;
+            }
 
-                transition: 150ms ease-in;
-                text-transform: uppercase;
+            .additional {
+                display: block;
+                font-weight: 300;
+                font-size: .85em;
+                margin-bottom: 3em;
+                max-width: 60%;
+                line-height: 150%;
+                color: #94A3B8;
+            }
 
-                &::before {
-                    content: "";
-                    height: 1px;
-                    width: 2em;
-                    background-color: #475569;
-                    position: absolute;
-                    top: 50%;
-                    left: 0;
-                    transition: 150ms ease-in;
+            .nav-list {
+                .nav-item {
+                    list-style: none;
+                    margin-bottom: .5em;
+
+                    .link {
+                        position: relative;
+
+                        cursor: pointer;
+
+                        color: #64748B;
+                        font-size: .65em;
+                        font-weight: 700;
+                        letter-spacing: .15em;
+
+                        padding: 1em .5em 1em 3em;
+
+                        transition: 150ms ease-in;
+                        text-transform: uppercase;
+
+                        &::before {
+                            content: "";
+                            height: 1px;
+                            width: 2em;
+                            background-color: #475569;
+                            position: absolute;
+                            top: 50%;
+                            left: 0;
+                            transition: 150ms ease-in;
+                        }
+
+                        &:hover, &:focus {
+                            padding-left: 5em;
+                            color: #E2E8F0;
+
+                            &::before {
+                                width: 4em;
+                                background-color: #E2E8F0;
+                            }
+                        }
+                    }
                 }
+            }
+        }
 
-                &:hover, &:focus {
-                    padding-left: 5em;
-                    color: #E2E8F0;
-
-                    &::before {
-                        width: 4em;
-                        background-color: #E2E8F0;
+        .footer {
+            min-height: 3em;
+            width: 100%;
+            
+            .list {
+                display: flex;
+                align-items: center;
+                
+                .list-item {
+                    list-style: none;
+                    margin-right: .5em;
+                    
+                    .link {
+                        .icon {
+                            color: #94A3B8;
+                            font-size: 1.75em;
+                            transition: 150ms ease-in;
+                            
+                            &.gh {
+                                margin-top: -.075em;
+                                font-size: 1.6em;
+                            }
+                        }
+                        
+                        &:hover {
+                            .icon {
+                                color: #E2E8F0;
+                            }
+                        }
                     }
                 }
             }
